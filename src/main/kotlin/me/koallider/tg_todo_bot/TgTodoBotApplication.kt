05aab7457632a8f92d823b71667715cecbfad358
koallider <xehxtkf@gmail.com>
2022-12-25 12:54:22 +0300

    added mongo dependency

--- src/main/kotlin/me/koallider/tg_todo_bot/TgTodoBotApplication.kt
+++ src/main/kotlin/me/koallider/tg_todo_bot/TgTodoBotApplication.kt
@@ -1,6 +1,5 @@
 package me.koallider.tg_todo_bot
 
-import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
@@ -8,7 +7,5 @@ import org.springframework.boot.runApplication
 class TgTodoBotApplication
 
 fun main(args: Array<String>) {
-    println("MAIN")
-    //runApplication<TgTodoBotApplication>(*args)
-    SpringApplication.run(TgTodoBotApplication::class.java)
+    runApplication<TgTodoBotApplication>(*args)
 }
